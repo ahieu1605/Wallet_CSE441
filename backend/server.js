e714545ca8579fbd0da1b8726c5e2bc193339019
thucvanminh <thucnichan2929vmt@gmail.com>
2025-06-14 16:31:34 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -17,43 +17,43 @@ app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 
-async function initDB() {
-    try {
-        await sql`CREATE TABLE IF NOT EXISTS transactions
-        (
-            id
-            SERIAL
-            PRIMARY
-            KEY,
-            user_id
-            VARCHAR
-                  (
-            255
-                  ) NOT NULL,
-            title VARCHAR
-                  (
-                      255
-                  ) NOT NULL,
-            amount DECIMAL
-                  (
-                      10,
-                      2
-                  )
-            NOT NULL,
-            category VARCHAR
-                  (
-                      255
-                  ) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-            )`;
-        console.log("Database initialized successfully");
-    } catch (error) {
-        console.log("Error initializing database:", error);
-        process.exit(1);
-
-    }
-}
-
+// async function initDB() {
+//     try {
+//         await sql`CREATE TABLE IF NOT EXISTS transactions
+//         (
+//             id
+//             SERIAL
+//             PRIMARY
+//             KEY,
+//             user_id
+//             VARCHAR
+//                   (
+//             255
+//                   ) NOT NULL,
+//             title VARCHAR
+//                   (
+//                       255
+//                   ) NOT NULL,
+//             amount DECIMAL
+//                   (
+//                       10,
+//                       2
+//                   )
+//             NOT NULL,
+//             category VARCHAR
+//                   (
+//                       255
+//                   ) NOT NULL,
+//             created_at DATE NOT NULL DEFAULT CURRENT_DATE
+//             )`;
+//         console.log("Database initialized successfully");
+//     } catch (error) {
+//         console.log("Error initializing database:", error);
+//         process.exit(1);
+//
+//     }
+// }
+//
 
 
 
@@ -62,13 +62,15 @@ async function initDB() {
 
 app.use("/api/transactions",transactionsRoute);
 // app.use("/api/products",transactionsRoute);
-initDB().then(()=>{
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-        console.log(`http://localhost:${PORT}`);
-    })
+// initDB().then(()=>{
+//     app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`);
+//         console.log(`http://localhost:${PORT}`);
+//     })
+// });
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
-// app.listen(5001, () => {
-//     console.log("Server running on port 5001");
-//     console.log("http://localhost:5001");
-// })
